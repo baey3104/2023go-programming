9191e9a67353c23c79f37074eaffe921ac21badd
neullxh21 <neullxh21@gmail.com>
2023-10-23 12:11:56 +0900

    week9 v1.3

--- week9/main.go
+++ week9/main.go
@@ -2,11 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
-	var a int = 10
-	var pa *int
-	pa = &a
+// pass by pointer (call by pointer)
+func swap(n1 *int, n2 *int) { // 두 수의 교환
+	temp := *n1
+	*n1 = *n2
+	*n2 = temp
+}
 
-	fmt.Println(&a, a)
-	fmt.Println(pa, *pa)
+func main() {
+	a := 10
+	b := 20
+
+	c := &a               // integer 타입의 변수 주소는 integer 타입의 포인터 변수로만 받을 수 있다.
+	fmt.Printf("%T\n", c) // *int
+
+	fmt.Println(a, b)
+	swap(&a, &b)
+	fmt.Println(a, b)
 }
